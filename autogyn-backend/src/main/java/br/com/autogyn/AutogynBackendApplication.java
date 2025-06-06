ce14ee1d58c8188408fa9c0e2f49e45f2bd60627
Gabriel Nunes <gabrielnunes.service@gmail.com>
2025-06-06 12:50:55 -0300

    Primeiro commit

---
+++ autogyn-backend/src/main/java/br/com/autogyn/AutogynBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.autogyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogynBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutogynBackendApplication.class, args);
+	}
+
+}
---
+++ autogyn-backend/src/test/java/br/com/autogyn/AutogynBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autogyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutogynBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
