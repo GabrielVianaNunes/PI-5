42510893a39684f9fd0c68d61c56f2c22e04ce0c
Gabriel Nunes <gabrielnunes.service@gmail.com>
2025-06-07 19:28:20 -0300

    app.config.ts alterado para o projeto, components de clientes e veiculos criados.

--- autogyn-frontend/src/app/app.config.ts
+++ autogyn-frontend/src/app/app.config.ts
@@ -1,12 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+// Módulos Angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Módulos PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule,
+      ButtonModule,
+      InputTextModule,
+      TableModule,
+      DropdownModule,
+      CalendarModule,
+      ToastModule
+    )
   ]
 };
---
+++ autogyn-frontend/src/app/components/veiculos/veiculo-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veiculo-list',
+  imports: [],
+  templateUrl: './veiculo-list.html',
+  styleUrl: './veiculo-list.css'
+})
+export class VeiculoList {
+
+}
