e17bcb2f6e8c75695376d86343a05407a197ebc1
Gabriel Nunes <gabrielnunes.service@gmail.com>
2025-06-07 13:04:00 -0300

    OrdemDeServicoDTO criada.

---
+++ autogyn-backend/src/main/java/br/com/autogyn/dto/OrdemDeServicoDTO.java
@@ -0,0 +1,77 @@
+package br.com.autogyn.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+
+public class OrdemDeServicoDTO {
+
+    @NotNull(message = "ID do veículo é obrigatório")
+    private Long veiculoId;
+
+    @NotNull(message = "Data de abertura é obrigatória")
+    @FutureOrPresent(message = "A data de abertura não pode ser no passado")
+    private LocalDate dataAbertura;
+
+    private LocalDate dataFechamento;
+
+    @NotNull(message = "Status é obrigatório")
+    private String status; // ABERTA, EM_ANDAMENTO, CONCLUIDA, CANCELADA
+
+    @NotNull(message = "Lista de itens é obrigatória")
+    private List<ItemOrdemServicoDTO> itens;
+
+    public OrdemDeServicoDTO() {
+    }
+
+    public OrdemDeServicoDTO(Long veiculoId, LocalDate dataAbertura, LocalDate dataFechamento, String status,
+            List<ItemOrdemServicoDTO> itens) {
+        this.veiculoId = veiculoId;
+        this.dataAbertura = dataAbertura;
+        this.dataFechamento = dataFechamento;
+        this.status = status;
+        this.itens = itens;
+    }
+
+    public Long getVeiculoId() {
+        return veiculoId;
+    }
+
+    public void setVeiculoId(Long veiculoId) {
+        this.veiculoId = veiculoId;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDate getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDate dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<ItemOrdemServicoDTO> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemOrdemServicoDTO> itens) {
+        this.itens = itens;
+    }
+}
